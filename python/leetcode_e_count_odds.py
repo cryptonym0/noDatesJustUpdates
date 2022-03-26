7aed1b2d8e825e3e5464bd5e4e7481a677920b07
Elycia Finch <elyciabuckley@gmail.com>
2022-03-26 13:35:48 -0700

    Create leetcode_e_count_odds.py

---
+++ python/leetcode_e_count_odds.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/submissions/detail/667791407/
+
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        #both numbers odd
+        #first number odd second even
+        #first number even second odd
+        #both even
+        if(low % 2 == 0):
+            low += 1
+        if(high % 2 == 0):
+            high -= 1
+        return (high-low) // 2 + 1
