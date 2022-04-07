38d0db506e5a5384e13eb1d7b6002b3893ca8148
Elycia Finch <elyciabuckley@gmail.com>
2022-04-07 16:56:25 -0700

    Create leetcode_e_reverse_bits.py

---
+++ python/leetcode/leetcode_e_reverse_bits.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/submissions/detail/676025861/
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        #ok
+        #I can do this
+        #i remeber sharma making me do this by hand
+        #I am the algorithm
+        length = 32
+        binary = bin(n)[2:].rjust(length,'0')[::-1]
+        return int(binary,2)
+        #holy shit
+        
+
+#         i = 0
+#         for b in n:
+#             i = i**2 + int(b)
+        
